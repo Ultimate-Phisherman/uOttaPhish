89e37023facd0a5c5a0df80754a38bd2c1f74877
Ultimate-Phisherman <das00018@algonquinlive.com>
2026-01-18 09:47:32 -0500

    Delete uOttaFish directory

--- uOttaFish/alert.js
+++
@@ -1,9 +0,0 @@
-chrome.storage.local.get("phishingData", (result) => {
-    const data = result.phishingData;
-
-    document.getElementById("title").textContent = "This email is a\nPHISHING email!";
-    document.getElementById("title").classList.add("phishing");
-    document.getElementById("description").textContent = data.description;
-
-    document.getElementById("helpBtn").style.display = "block";
-});
